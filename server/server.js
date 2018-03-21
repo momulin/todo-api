af20bb35e5ce0ef0be572cd4f1c952c600af81d9
momulin <jles87006@gmail.com>
2018-03-21 20:52:32 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -29,6 +29,19 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+      res.send({todo});
+
+  }).catch((e)=>res.status(404).send());
+
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
