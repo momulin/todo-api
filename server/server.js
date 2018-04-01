35a27af8db213399eb26d1a0d2084f6ca1d03925
momulin <jles87006@gmail.com>
2018-04-01 19:27:03 +0800

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ app.use(bodyParser.json());
 
 //TODOS
 
-app.post('/todos', (req, res) => {
+app.post('/todos',authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator:req.user._id
   });
 
   todo.save().then((doc) => {
@@ -30,18 +31,23 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+  Todo.find({
+    _creator:req.user._id
+  }).then((todos)=>{
     res.send({todos});
   },(e)=>{
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
   var id = req.params.id;
 
-  Todo.findById(id).then((todo)=>{
+  Todo.findOne({
+    _id:id,
+    _creator:req.user._id
+  }).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
@@ -51,9 +57,12 @@ app.get('/todos/:id',(req,res)=>{
 
 });
 
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
   var id = req.params.id;
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator:req.user._id
+  }).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
@@ -61,7 +70,7 @@ app.delete('/todos/:id',(req,res)=>{
   }).catch((e)=>res.status(404).send());
 });
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
   var id = req.params.id;
   var body = _.pick(req.body,['text','completed']);
 
@@ -76,7 +85,10 @@ app.patch('/todos/:id',(req,res)=>{
    body.completedAt = null;
    }
 
-   Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then((todo)=>{
+   Todo.findOneAndUpdate({
+     _id:id,
+     _creator:req.user._id
+   },{$set: body},{new:true}).then((todo)=>{
      if(!todo){
        return res.status(404).send();
      }
